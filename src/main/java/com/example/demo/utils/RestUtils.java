c023f270d7a95ad11976dae2c94e9160e4241c6e
YT24 <54975784+YT24@users.noreply.github.com>
2019-09-11 14:20:59 +0800

    Update RestUtils.java

--- src/main/java/com/example/demo/utils/RestUtils.java
+++ src/main/java/com/example/demo/utils/RestUtils.java
@@ -11,12 +11,19 @@ public class RestUtils {
 		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
 		
 		
-<<<<<<< HEAD
-		System.out.println("11111111111111111111111");
 
-=======
+
+	}
+	
+	public static void get(String str) {
+
+		System.out.println("dsgdkj");
+		System.out.println("dsafdfgfhg");System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
+		System.out.println("jjjjjjjjjjjjj");
+		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
 		
-		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjqwetrytruyjjjjjjj");
->>>>>>> branch 'master' of https://github.com/YT24/Config-Server.git
+		
+
+
 	}
 }
