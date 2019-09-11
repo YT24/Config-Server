8fe828a1c987a9756f3915c6b7d0dd38682009c4
yangte66 <yangt@paraview.cn>
2019-09-11 14:21:29 +0800

    12

--- src/main/java/com/example/demo/utils/RestUtils.java
+++ src/main/java/com/example/demo/utils/RestUtils.java
@@ -11,12 +11,28 @@ public class RestUtils {
 		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
 		
 		
-<<<<<<< HEAD
+
 		System.out.println("11111111111111111111111");
 
-=======
+
 		
 		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjqwetrytruyjjjjjjj");
->>>>>>> branch 'master' of https://github.com/YT24/Config-Server.git
+	}
+	
+	
+	public static void get1() {
+
+		System.out.println("dsgdkj");
+		System.out.println("dsafdfgfhg");System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
+		System.out.println("jjjjjjjjjjjjj");
+		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjjjjjjjj");
+		
+		
+
+		System.out.println("11111111111111111111111");
+
+
+		
+		System.out.println("jjjjjjjjjjjjj");System.out.println("jjjjjjqwetrytruyjjjjjjj");
 	}
 }
