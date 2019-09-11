f7b0156b6e39ba10ec9e56d8abb17853f07b715c
YT24 <54975784+YT24@users.noreply.github.com>
2019-09-11 14:03:47 +0800

    Update Test.java

--- src/main/java/com/example/demo/utils/Test.java
+++ src/main/java/com/example/demo/utils/Test.java
@@ -5,4 +5,14 @@ public class Test {
 	public static void main(String[] args) {
 		System.err.println("ddddddddddddddddd");
 	}
+	
+	public static void main(String[] args) {
+		System.err.println("ddddddddddddddddd");
+	}
+	public static void main(String[] args) {
+		System.err.println("ddddddddddddddddd");
+	}
+	public static void main(String[] args) {
+		System.err.println("ddddddddddddddddd");
+	}
 }
