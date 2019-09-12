8ba70b1b016992acf9810799b24be481be9e8172
yangte66 <yangt@paraview.cn>
2019-09-12 17:29:32 +0800

    123

--- src/main/java/com/example/demo/EurekaConfigApplication.java
+++ src/main/java/com/example/demo/EurekaConfigApplication.java
@@ -2,25 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaConfigApplication.class, args);
 	}
-	public static void main2(String[] args) {
-		SpringApplication.run(EurekaConfigApplication.class, args);
 
-
-		System.out.println("1111111111112222222222222222222222222222222222222222111111111111hahahahahahahhahah");
-
-	}
-
-
-	public static void say(){
-		System.out.println("nihao");
-	}
 }
