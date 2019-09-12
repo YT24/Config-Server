abb05626d0dd39b714a9c9173cc8419a4abc38ba
yangte66 <yangt@paraview.cn>
2019-09-12 14:39:45 +0800

    update4

--- src/main/java/com/example/demo/utils/Ggg.java
+++ src/main/java/com/example/demo/utils/Ggg.java
@@ -1,26 +1,5 @@
 package com.example.demo.utils;
 
 public class Ggg {
-    private String username;
-    private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Ggg(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "Ggg{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
